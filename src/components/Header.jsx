701d557417cb309e6a955386f84389948fbe96bb
PacifistRam <141394720+PacifistRam@users.noreply.github.com>
2024-05-15 11:46:24 +0530

    intial setup and tailwind integration

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+const Header = () => {
+  return (
+    <div className=" py-8 px-4 mb-8">
+        <div className="flex justify-between">
+            <h1 className="text-4xl font-bold py-3 px-4">Memory Game</h1>
+            <div className="py-3 px-4 text-xl">
+                <p>Your Score:</p>
+                <p>High Score:</p>
+            </div>
+        </div>
+            <p className="font-medium capitalize">get points for clicking on a card but don't click on anymore than once </p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+
+
+const MainComponent = () => {
+  return (
+    <div className="max-w-[1440px] m-auto bg-slate-400 text-slate-100
+    min-h-screen ">
+        <Header />
+    </div>
+  )
+}
+
+export default MainComponent
